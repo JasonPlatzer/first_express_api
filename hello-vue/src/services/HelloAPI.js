81b555aa30f7175e359b35302acc9a49a3577841
frostbite169 <44807903+frostbite169@users.noreply.github.com>
2021-04-13 14:51:14 -0500

    tried a fix123

--- hello-vue/src/services/HelloAPI.js
+++ hello-vue/src/services/HelloAPI.js
@@ -4,7 +4,7 @@ let base = 'api'
 // export default means provide a function
 export default {
     getHelloMessage(){
-        // the axios call aaa
+        // the axios call   ggggg
         return axios.get(base).then(response => {
             return response.data
         })
